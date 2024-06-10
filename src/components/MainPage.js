c8703c3a057935324ade8ebd4fdae30fc249ae1a
Hiru1003 <hirumiperera10@gmail.com>
2024-06-10 11:44:19 +0530

    feat: change bg color

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -5,47 +5,70 @@ import { FaCircleArrowUp } from "react-icons/fa6";
 
 const MainPage = () => {
   return (
-    <Box sx={{ pl: 10, pr:10, pt:2, pb:2, flexGrow: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-      {/* Containers Row */}
+    <Box sx={{ 
+      pl: 10, 
+      pr:10, 
+      pt:2, 
+      pb:2, 
+      flexGrow: 1, 
+      display: 'flex', 
+      flexDirection: 'column', 
+      justifyContent: 'space-between' ,
+      bgcolor: 'grey.900',}}>
+
       <Box
         sx={{
           display: 'flex',
           gap: 2,
-          p: 2,
+          p: 4,
         }}
       >
         {/* Container 1 */}
-        <Box sx={{ flex: 1, textAlign: 'center', border: '1px solid black' }}>
-          <Typography variant="h6">Container 1</Typography>
-          <Typography variant="body1">Example content here</Typography>
+        <Box sx={{ flex: 1, textAlign: 'center', border: '1px solid grey', borderRadius:'5px'}}>
+          <Typography variant="h6" sx={{color:"grey" }}>Container 1</Typography>
+          <Typography variant="body1" sx={{color:"grey" }}>Example content here</Typography>
         </Box>
 
         {/* Container 2 */}
-        <Box sx={{ flex: 1, textAlign: 'center', border: '1px solid black' }}>
-          <Typography variant="h6">Container 2</Typography>
-          <Typography variant="body1">Example content here</Typography>
+        <Box sx={{ flex: 1, textAlign: 'center', border: '1px solid grey', borderRadius:'5px' }}>
+          <Typography variant="h6" sx={{color:"grey" }}>Container 2</Typography>
+          <Typography variant="body1" sx={{color:"grey" }}>Example content here</Typography>
         </Box>
 
         {/* Container 3 */}
-        <Box sx={{ flex: 1, textAlign: 'center', border: '1px solid black' }}>
-          <Typography variant="h6">Container 3</Typography>
-          <Typography variant="body1">Example content here</Typography>
+        <Box sx={{ flex: 1, textAlign: 'center', border: '1px solid grey', borderRadius:'5px' }}>
+          <Typography variant="h6" sx={{color:"grey" }}>Container 3</Typography>
+          <Typography variant="body1" sx={{color:"grey" }}>Example content here</Typography>
         </Box>
 
         {/* Container 4 */}
-        <Box sx={{ flex: 1, textAlign: 'center', border: '1px solid black' }}>
-          <Typography variant="h6">Container 4</Typography>
-          <Typography variant="body1">Example content here</Typography>
+        <Box sx={{ flex: 1, textAlign: 'center', border: '1px solid grey', borderRadius:'5px' }}>
+          <Typography variant="h6" sx={{color:"grey" }}>Container 4</Typography>
+          <Typography variant="body1" sx={{color:"grey" }}>Example content here</Typography>
         </Box>
       </Box>
 
       {/* Text Field and IconButton */}
-      <Box sx={{ mt: 2, display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-        <TextField variant="outlined" fullWidth placeholder="Type your message..." sx={{ width: 'calc(100% - 55px)', mr: 1 }} />
-        <IconButton  aria-label="send" sx={{ fontSize: '2.5rem', color:"black" }}>
-          <FaCircleArrowUp />
-        </IconButton>
-      </Box>
+      <Box sx={{ mt: 2, display: 'flex', flexDirection: 'row', alignItems: 'center', padding: '10px' }}>
+      <TextField 
+        fullWidth 
+        placeholder="Type your message..." 
+        sx={{ 
+          width: 'calc(100% - 55px)', 
+          mr: 1, 
+          bgcolor: '#333', 
+          color: 'white', 
+          '::placeholder': { 
+            color: 'white' 
+          } 
+        }} 
+/><IconButton aria-label="send" sx={{ fontSize: '2.5rem', color: "white" }}>
+        <FaCircleArrowUp />
+      </IconButton>
+    </Box>
+
+
+
     </Box>
   );
 }
