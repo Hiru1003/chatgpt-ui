58f5bd56bf38876de7e336822463ed3b9d1b74f3
Hiru1003 <hirumiperera10@gmail.com>
2024-06-12 13:18:44 +0530

    refactor: remove footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { BsStars } from 'react-icons/bs'; 
 import { Box, Typography } from '@mui/material';
+import { FaQuestionCircle } from "react-icons/fa";
+
 
 const SidebarFooter = () => {
   return (
-    <Box sx={{ bgcolor: 'black', p: 2, textAlign: 'left', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-      <Box sx={{ color: 'white', mr: 3 }}>
-        <BsStars style={{ fontSize: '1.5rem', }} />
-      </Box>
-      <Box sx={{ color: 'white', }}>
-        <Typography style={{color: 'white' , fontSize: '1.2rem'}}>Upgrade Plan<br/></Typography>
-        <Typography   style={{color: 'white', fontSize: '0.9rem' }}>Get GPT-4, DALL-E, and more<br/></Typography>
-     </Box>
+    <Box>
+
     </Box>
+    
   );
 }
 
