1fcb49ea142b4e6e503da707f5d4f804a5ab9813
Hiru1003 <hirumiperera10@gmail.com>
2024-06-12 08:09:32 +0530

    feat: sidebar hidden function reamke

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import IconButton from '@mui/material/IconButton';
 import Sidebar from './components/Sidebar';
 import MainPage from './components/MainPage';
 import LoginPage from './components/Login';
@@ -8,14 +9,27 @@ import MessagePage from './components/Message';
 import SignupPage from './components/SignUp';
 import ForgotPasswordPage from './components/ForgotPassword'; 
 import { Routes, Route, useLocation } from 'react-router-dom';
+import { BsReverseLayoutTextSidebarReverse } from "react-icons/bs";
 
 const theme = createTheme({
   palette: {
+    background: {
+      default: '#121212' // Set the default background color to a dark theme
+    },
     primary: {
       main: '#1976d2',
     },
     secondary: {
       main: '#dc004e',
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          backgroundColor: '#121212', // Ensure the body background color is consistent
+        },
+      },
     },
   },
 });
@@ -30,18 +44,38 @@ function App() {
     setSidebarVisible(shouldShowSidebar);
   }, [location]);
 
+  const handleToggleSidebar = () => {
+    setSidebarVisible(prevState => !prevState);
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div style={{ display: 'flex', height: '100vh' }}>
+      <div style={{ display: 'flex', height: '100vh', backgroundColor: '#121212' }}>
+        {/* Sidebar Toggle Button */}
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={handleToggleSidebar}
+          sx={{ position: 'absolute', top: 16, left: 16, zIndex: 1300, color: 'white' }}
+        >
+          <BsReverseLayoutTextSidebarReverse fontSize="x-large" />
+        </IconButton>
+
+        
+
+        {/* Sidebar */}
         {isSidebarVisible && (
           <Sidebar isVisible={isSidebarVisible} />
         )}
-        <div style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+
+        {/* Main Content */}
+        <div style={{ display: 'flex', flexDirection: 'column', flexGrow: 1, marginLeft: isSidebarVisible ? '0px' : '0', backgroundColor: '#121212' }}>
           <Routes>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
-            <Route path="/forgot-password" element={<ForgotPasswordPage />} /> {/* Route for the forgot password page */}
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} /> 
             <Route path="/message" element={<MessagePage />} />
             <Route path="/" element={<MainPage />} />
           </Routes>
