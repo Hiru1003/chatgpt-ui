76b2786fec4286757e3868e734e4cec7045acacd
Hiru1003 <hirumiperera10@gmail.com>
2024-06-17 09:37:39 +0530

    feat: static footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,24 @@ import { FaQuestionCircle } from "react-icons/fa";
 const SidebarFooter = () => {
   return (
     <Box>
+            <Box sx={{ bgcolor: 'black', p: 2, textAlign: 'left', display: 'flex', flexDirection: 'row', alignItems: 'left', justifyContent: 'left' }}>
+              <Box sx={{ color: 'white', mr: 3 }}>
+                <BsStars style={{ fontSize: '1.2rem' }} />
+              </Box>
+              <Box sx={{ color: 'white' }}>
+                <Typography style={{ color: 'white', fontSize: '1.1rem' }}>Explore New Release<br /></Typography>
+              </Box>
+            </Box>
 
-    </Box>
+            <Box sx={{ bgcolor: 'black', p: 2, textAlign: 'left', display: 'flex', flexDirection: 'row', alignItems: 'left', justifyContent: 'left' }}>
+              <Box sx={{ color: 'white', mr: 3 }}>
+                <FaQuestionCircle style={{ fontSize: '1.2rem' }} />
+              </Box>
+              <Box sx={{ color: 'white' }}>
+                <Typography style={{ color: 'white', fontSize: '1.1rem' }}>Get Help<br /></Typography>
+              </Box>
+            </Box>
+          </Box>
     
   );
 }
