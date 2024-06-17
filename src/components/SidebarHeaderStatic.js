0c469187a4ed47ffa7cf4a4cd86f995aeb1dc2c1
Hiru1003 <hirumiperera10@gmail.com>
2024-06-17 09:37:28 +0530

    feat: static header

---
+++ src/components/SidebarHeaderStatic.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BsStars } from 'react-icons/bs'; 
+import { Box, Typography } from '@mui/material';
+
+const SidebarHeaderStatic = () => {
+  return (
+    <div>
+          <Box>
+            <Box sx={{ bgcolor: 'black', p: 2, textAlign: 'left', display: 'flex', flexDirection: 'row', alignItems: 'left', justifyContent: 'left' }}>
+              <Box sx={{ color: 'white', mr: 3 }}>
+                <BsStars style={{ fontSize: '1.2rem' }} />
+              </Box>
+              <Box sx={{ color: 'white' }}>
+                <Typography style={{ color: 'white', fontSize: '1.1rem' }}>GTP Pro<br /></Typography>
+              </Box>
+            </Box>
+          </Box>
+    </div>
+  )
+}
+
+export default SidebarHeaderStatic
