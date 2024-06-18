9aafb25fa077d79b8317c6d88618f03c2a9baf5e
Hiru1003 <hirumiperera10@gmail.com>
2024-06-18 10:44:41 +0530

    feat: sidebar visibility change

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,20 +1,18 @@
 import React, { useState, useRef } from 'react';
-import { Box, IconButton, TextField,Typography } from '@mui/material';
-import { FaCircleArrowUp } from "react-icons/fa6";
+import { Box} from '@mui/material';
 import MainpageContainer from './MainpageContainer';
 import { SiOpenai } from "react-icons/si";
 import ChatgptDropdownHeader from './ChatgptDropdownHeader';
-import { CgAttachment } from "react-icons/cg";
-import { MdKeyboardVoice } from "react-icons/md";
-import { LiaBookSolid } from "react-icons/lia";
-import UploadForm from './UploadForm';
-import QuestionMarkDropdown from './QuestionMarkDropdown';
-
+import { useTheme, useMediaQuery } from '@mui/material';
+import TextAreaTemplete from './TextArea';
 
 const MainPage = () => {
   const [showForm, setShowForm] = useState(false);
   const [activeForm, setActiveForm] = useState('yourPrompts');
   const fileInputRef = useRef(null);
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
 
   const handleUploadClick = () => {
     setShowForm(true);
@@ -48,9 +46,9 @@ const MainPage = () => {
 
   return (
     <Box sx={{ 
-      height: 'calc(100% - 50px)',
-      pl: 10, 
-      pr: 10, 
+      height: '100vh',
+      pl: { xs: 2, sm: 10 },
+      pr: { xs: 2, sm: 10 },
       pt: 1, 
       pb: 3, 
       flexGrow: 1, 
@@ -59,116 +57,24 @@ const MainPage = () => {
       justifyContent: 'space-between',
       bgcolor: 'grey.900',
       position: 'relative',
+      overflowX: 'hidden', 
     }}>
 
       {/* Main header */}
-      <Box>
-        <ChatgptDropdownHeader/>
-      </Box>
+        <ChatgptDropdownHeader />
       
       {/* OpenAI Icon */}
       <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <SiOpenai size={70} style={{ color: 'white' }} />
-      </Box>
+      <SiOpenai size={isSmallScreen ? 30 : 70} style={{ color: 'white' }} />
+     </Box>
 
       {/* Main page container */}
-      <Box sx={{ display: 'flex', gap: 2}}>
+      <Box sx={{ display: 'flex', gap: 2, flexDirection: { xs: 'column', sm: 'row' } }}>
         <MainpageContainer />
       </Box>
-
-       {/* Text feild container */}
-      <Box sx={{ 
-        position: 'absolute', 
-        bottom: 45, 
-        left: '50%',
-        transform: 'translateX(-50%)',
-        width: '70%', 
-        display: 'flex', 
-        flexDirection: 'row', 
-        alignItems: 'center', 
-        justifyContent: 'center', 
-        padding: '10px', 
-        color: 'white',
-      }}>
-        <IconButton aria-label="upload" sx={{ fontSize: '2.5rem', color: "white" }} onClick={handleUploadClick}>
-          <LiaBookSolid />
-        </IconButton>
-        <IconButton aria-label="upload" sx={{ fontSize: '2rem', color: "white" }} onClick={handleFileUploadButtonClick}>
-          <CgAttachment />
-        </IconButton>
-
-        <TextField 
-          fullWidth 
-          placeholder="Message ChatGPT" 
-          sx={{ 
-            width: '100%',
-            mr: 1, 
-            border: '1px solid black', 
-            borderRadius: '15px', 
-            bgcolor: '#333', 
-            color: 'white',
-            '::placeholder': { 
-              color: 'white',
-              animation: 'blink-caret 0.75s step-end infinite'
-            } 
-          }} 
-          InputProps={{ style: { color: 'white' } }}
-        />
-        
-        <IconButton aria-label="send" sx={{ fontSize: '2.2rem', color: "white" }}>
-          <MdKeyboardVoice />
-        </IconButton>
-        <IconButton aria-label="send" sx={{ fontSize: '2.5rem', color: "white" }}>
-          <FaCircleArrowUp />
-        </IconButton>
-        
-        <input 
-          type="file" 
-          ref={fileInputRef} 
-          style={{ display: 'none' }} 
-          onChange={handleFileUpload} 
-        />
-      </Box>
-      
-      <Box sx={{ position: 'relative',bottom: 0,top: 165, display: 'flex', alignItems: 'center', justifyContent: 'center', }}>
-        <Box>
-          <Typography sx={{ color: "grey", fontSize: '16px', textAlign:'center' }}>ChatGPT can make mistakes. Check important info.</Typography>
-        </Box>
-      </Box>
-
-      <QuestionMarkDropdown/>
-
-
-
-
-
-      {/* Forms */}
-      {showForm && (
-        <>
-          <Box 
-            sx={{
-              position: 'fixed',
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              bgcolor: 'rgba(0, 0, 240, 0.05)',
-              zIndex: 1200,
-              backdropFilter: 'blur(2px)', 
-            }}
-            onClick={handleFormClose}
-          />
-          <UploadForm 
-            onClose={handleFormClose} 
-            activeForm={activeForm} 
-            switchToYourPrompts={switchToYourPrompts}
-            switchToCommunityPrompts={switchToCommunityPrompts}
-          />
-        </>
-      )}
-
+         
+      <TextAreaTemplete/>
     </Box>
-    
   );
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,9 +4,9 @@ import { BsReverseLayoutTextSidebarReverse } from "react-icons/bs";
 import { FaRegPenToSquare } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 import { FaQuestionCircle } from "react-icons/fa";
-import ChatHistory from './ChatHistory';
+import ChatHistory from './ChatHistory'
 import { BsStars } from 'react-icons/bs'; 
-import SidebarFooter from './Footer';
+import SidebarFooter from './Footer'
 import SidebarHeaderStatic from './SidebarHeaderStatic';
 
 const Sidebar = ({ isVisible, onToggleSidebar }) => {
@@ -17,8 +17,8 @@ const Sidebar = ({ isVisible, onToggleSidebar }) => {
         flexDirection: 'column',
         height: '100vh',
         bgcolor: 'black',
-        width: isVisible ? '350px' : '50px',
-        transition: 'width 0.3s ease',
+        width: '350px',
+        ...(isVisible ? { width: '350px' } : { width: '50px' }),
       }}
     >
       <Box
@@ -29,67 +29,67 @@ const Sidebar = ({ isVisible, onToggleSidebar }) => {
           alignItems: 'center',
           padding: '8px',
           paddingBottom: '10px',
-          width: '100%',
+          width: '350px',
+        ...(isVisible ? { width: '350px' } : { width: '50px' }),
         }}
       >
         <IconButton onClick={onToggleSidebar} style={{ color: 'grey' }}>
           <BsReverseLayoutTextSidebarReverse fontSize="1.3rem" />
         </IconButton>
         <Link to="/">
-          <IconButton style={{ color: 'grey' }}>
-            <FaRegPenToSquare fontSize="1.3rem" />
-          </IconButton>
-        </Link>
+              <IconButton style={{ color: 'grey' }}>
+                <FaRegPenToSquare fontSize="1.3rem" />
+              </IconButton></Link>
         {!isVisible && (
           <>
-            <IconButton style={{ color: 'grey', position: 'fixed', bottom: '45px' }}>
+            <IconButton style={{ color: 'grey',position: 'fixed',bottom: '45px', }}>
               <BsStars fontSize="1.2rem" />
             </IconButton>
-            <IconButton style={{ color: 'grey', position: 'fixed', bottom: '10px' }}>
+            <IconButton style={{ color: 'grey',position: 'fixed', bottom: '10px', }}>
               <FaQuestionCircle fontSize="1.2rem" />
             </IconButton>
           </>
         )}
       </Box>
+      <SidebarHeaderStatic/>
+      {isVisible && (
+        <>
+          <div style={{ overflowY: 'auto', height: 'calc(100% - 50px - 60px)', scrollbarColor: '#666 #333', paddingLeft: '10px', paddingTop: '15px' }}>
+            <Box>
+            
+              <Box sx={{ paddingLeft: 1 }}>
+                <Typography variant="subtitle1" style={{ color: 'grey' }}>Today</Typography>
+              </Box>
 
-      <Box sx={{ position: 'relative', zIndex: 1 }}>
-        <SidebarHeaderStatic />
-      </Box>
+              <Link style={{ textDecoration: 'none' }} to='/DummyChat'> 
+                <ChatHistory text="Recipe for cake" />
+                <ChatHistory text="Coding with python" />
+                <ChatHistory text="React app with python" />
+                <ChatHistory text="How to make a diy table" />
+              </Link>
 
-      <div style={{ overflowY: 'auto', height: 'calc(100% - 50px - 60px)', scrollbarColor: '#666 #333', paddingLeft: '10px', paddingTop: '15px' }}>
-        <Box>
-          <Box sx={{ paddingLeft: 1 }}>
-            <Typography variant="subtitle1" style={{ color: 'grey' }}>Today</Typography>
-          </Box>
-          <Link style={{ textDecoration: 'none' }} to='/DummyChat'> 
-            <ChatHistory text="Recipe for cake" />
-            <ChatHistory text="Coding with python" />
-            <ChatHistory text="React app with python" />
-            <ChatHistory text="How to make a diy table" />
-          </Link>
-        </Box>
-        <Box sx={{ paddingLeft: 1 }}>
-          <Typography variant="subtitle1" style={{ color: 'grey' }}>Previous 7 Days</Typography>
-        </Box>
-        <Box>
-          <Link style={{ textDecoration: 'none' }} to='/DummyChat'>
-            <ChatHistory text="SE project ideas" />
-            <ChatHistory text="Remake the house style" />
-            <ChatHistory text="Breakfast ideas" />
-            <ChatHistory text="OOP concepts" />
-            <ChatHistory text="Meal plan generator" />
-            <ChatHistory text="Port change solution" />
-            <ChatHistory text="Breakfast ideas" />
-            <ChatHistory text="OOP concepts" />
-            <ChatHistory text="Meal plan generator" />
-            <ChatHistory text="Port change solution" />
-          </Link>
-        </Box>
-      </div>
-
-      <Box sx={{ position: 'relative', zIndex: 1 }}>
-        <SidebarFooter />
-      </Box>
+            </Box>
+            <Box sx={{ paddingLeft: 1 }}>
+              <Typography variant="subtitle1" style={{ color: 'grey' }}>Previous 7 Days</Typography>
+            </Box>
+            <Box>
+              <Link style={{ textDecoration: 'none' }} to='/DummyChat'>
+                <ChatHistory text="SE project ideas" />
+                <ChatHistory text="Remake the house style" />
+                <ChatHistory text="Breakfast ideas" />
+                <ChatHistory text="OOP concepts" />
+                <ChatHistory text="Meal plan generator" />
+                <ChatHistory text="Port change solution" />
+                <ChatHistory text="Breakfast ideas" />
+                <ChatHistory text="OOP concepts" />
+                <ChatHistory text="Meal plan generator" />
+                <ChatHistory text="Port change solution" />
+              </Link>
+            </Box>
+          </div>
+          <SidebarFooter />
+        </>
+      )}
     </Box>
   );
 };
